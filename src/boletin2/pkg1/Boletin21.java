2c6bfc6a2bae0475d9dccae2b7906e2f86b60c64
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2017-10-09 09:06:41 +0200

    Acabado

---
+++ src/boletin2/pkg1/Boletin21.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2.pkg1;
+
+/**
+ *
+ * @author ggarciaalvarez
+ */
+public class Boletin21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+        int a =4;
+        int b =3;
+        
+        int area =(a*b)/2;
+        System.out.println("area = "+area);
+        
+        
+    }
+    
+}
